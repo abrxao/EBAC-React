005df339153cbf6ce2ccc422e01bfc758f216fc6
Abraão Albuquerque <abraao.hs2@gmail.com>
2022-09-10 13:37:07 -0300

    consertando bug na seleção do checkbox

--- sintaxe-org/script.js
+++ sintaxe-org/script.js
@@ -5,23 +5,26 @@ const load = document.querySelector('.load');
 const form = document.querySelector('form');
 const table = document.querySelector('table');
 
+
+
 form.addEventListener('submit', (e) => { 
     e.preventDefault();
     load.style.display = 'block';
 
     var responseTable = "";
 
-    for(var i=0; i<8; i++) {
-        if(i>=5 && i<7){
-            if(e.target[i].checked){
-                responseTable += `<th>${e.target[i].value}</th>`; 
-            }
-
-        }else if(i==3){
-            responseTable += `<th>${e.target[i].options[e.target[i].selectedIndex].value}</th>`;
-            
-        }else{
+    for(var i=0; i<7; i++) {
+        if(e.target[i].checked){
             responseTable += `<th>${e.target[i].value}</th>`; 
+            console.log('condition1')
+        }
+        else if(i==3){
+            responseTable += `<th>${e.target[i].options[e.target[i].selectedIndex].value}</th>`;
+            console.log('condition2')
+            
+        }else if(i!=4 && i!=5){
+            responseTable += `<th>${e.target[i].value}</th>`;
+            console.log('condition3')
         }
         
     }
