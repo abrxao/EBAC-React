c8923c5d09f0fe327fc6543b3d7e033f9f7e0296
Abraão Albuquerque <abraao.hs2@gmail.com>
2022-12-19 20:14:36 -0300

    :lipstick: customizando bloco de mudancas

--- modulo-45/react-components/src/components/Header/index.js
+++ modulo-45/react-components/src/components/Header/index.js
@@ -1,21 +1,11 @@
-import React, {useState} from 'react';
-import logo from './../../logo.svg';
+import React from 'react';
+import './header.css';
 
 function Header(props){
 
-    const [count, setCount] = useState(1);
-
-    function increaseCount(){
-        setCount(count + 1);
-        console.log(count);
-    }
-    
     return(
         <header>
             <h1>{props.title}</h1>
-            <img src={logo}/>
-            <p>Bem vindo pela {count} vez</p>
-            <button onClick={increaseCount}>Incrementar</button>
         </header>
         )
     }
--- modulo-45/react-components/src/components/IMC/index.js
+++ modulo-45/react-components/src/components/IMC/index.js
@@ -2,31 +2,48 @@
  import './style.css';
  
  function IMC(){
-    const [weight, setWeight] = useState(0);
-    const [height, setHeight] = useState(0);
+    const [weight, setWeight] = useState();
+    const [height, setHeight] = useState();
     const [imc, setIMC] = useState(0);
     
     function calculator(){
-        setIMC(parseFloat(weight / (height * height)).toFixed(1));
-    }
+        if(weight.match(/[a-zA-Z]/) || height.match(/[a-zA-Z]/)){
+            alert('Verifique os valores dos campos');
+        }else{
+           setIMC(parseFloat(weight / (height * height)).toFixed(2)); 
+        }
+        
+    }    
+    
     return (
         <div className="IMC">
-        <h2>Cálculo IMC</h2>
-        <input 
-            type="text" 
-            placeholder="digite sua altura"
-            value={height}
-            onChange={e => setHeight(e.target.value)}
-            />
+            <h2>Cálculo IMC</h2>
+            
+            <div className="inputField">
+                <label for="height">Altura</label>
+                <input 
+                type="text" 
+                value={height}
+                onChange={e => setHeight(e.target.value)}
+                />
+            </div>
+            
+            <div className="inputField">
+            <label for="weight">Peso</label>  
+                <input 
+                type="text" 
+                value={weight}
+                onChange = {e => setWeight(e.target.value)}
+                />
+            </div>
+            
+            <button onClick={calculator} >Calcular</button>
+            
+            <div className="resultBlock">
+                <h4 >Resultado: {imc}</h4> 
+            </div>
         
-        <input 
-            type="text" 
-            placeholder="digite seu peso"
-            value={weight}
-            onChange = {e => setWeight(e.target.value)}
-        />
-        <button onClick={calculator} >Calcular</button>
-        <h4>Resultado: {imc}</h4> 
+        
         </div>
         )
     }
