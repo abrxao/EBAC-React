b7e3a44951e35a28168ec02f979f6f490435e0b6
Abraão Albuquerque <abraao.hs2@gmail.com>
2023-01-17 19:08:39 -0300

    :sparkles:feat:criando logica dentro de HOC para password

--- forms-with-hocs/pages/components/TextInput/TextInput.jsx
+++ forms-with-hocs/pages/components/TextInput/TextInput.jsx
@@ -1,12 +1,36 @@
-
 import withValidation from '../utils/withValidation';
+import React,{useState, useEffect} from 'react';
+import {IoMdEye,IoMdEyeOff} from 'react-icons/io'
 
-export function TextInput({Icon, name,onBlur}){
+export function TextInput({Icon, name, onKeyUp, valid, type},props){
+    const fnKeyUp = onKeyUp? onKeyUp: ()=>{};
+    const [isInputFill, setIsInputFill] = new useState(false);
+    const [inputType, setInputType] = new useState(type=="password" ? "password" : "text");
+
+    const handleKeyDown = (e)=>{
+        e.target.value ? setIsInputFill(true): setIsInputFill(false);
+    }
+
     return(
+        
         <div className='textInput'>
-            <input type="text" name={name} onBlur={e=>{onBlur(e.target.value)}}/>
-            <label forName={name}>{name[0].toUpperCase() + name.substring(1)}</label>
-            <span>{Icon}</span>
+
+            <input style={{
+                borderBottomColor: valid ? "red":"#fff"
+            }}
+            type={inputType}
+            id={name}
+            onChange={e=>handleKeyDown(e)}            
+            className={(isInputFill?"filled":"")}
+            onKeyUp={e=>{fnKeyUp(e.target.value)}}/>
+
+            <label htmlFor={name}><span className='textInput__icon'>{Icon}</span>{name[0].toUpperCase() + name.substring(1)}</label>
+
+            {name === "password" && <span className='textInput__viewPassword' onClick={()=>{setInputType(inputType=="password"?"text":"password")}}>{inputType=="password"?<IoMdEye/>:<IoMdEyeOff/>}</span>}
+
+            {name === "password" && <p className="textInput__passwordTip">
+                                    Password should have at <b>least 8 letters</b>, one <b>uppercase letter</b>, one <b>lowercase letter</b>, one special char and <b>one number</b></p> }
+            
         </div>
     )
 }
--- forms-with-hocs/pages/components/utils/withValidation.js
+++ forms-with-hocs/pages/components/utils/withValidation.js
@@ -1,65 +1,68 @@
 import {MdEmail} from 'react-icons/md';
 import {RiLockPasswordFill} from 'react-icons/ri';
 import {FaUser} from 'react-icons/fa';
+import React,{useState} from 'react';
 
-const inputTypes = {
-    email: {
-        icon:<MdEmail/>,
-        valid:()=>{},
-    },
-    password: {
-        icon:<RiLockPasswordFill/>,
-        valid:(password)=>{
-            {
-                const uppercase = /[A-Z]/;
-                const lowercase = /[a-z]/; 
-                const numbers = /[0-9]/;
-                const especialChars = /[!|@|#|$|%|^|&|*|(|)|-|_]/;
-                if(password.length > 8){
-                    console.log(false);
-                }
-                if(password.length < 8){
-                    console.log(false);
-                }
-                var auxUppercase = 0;
-                var auxLowercase = 0;
-                var auxNumber = 0;
-                var auxEspecial = 0;
-                for(var i=0; i<password.length; i++){
-                    if(uppercase.test(password[i]))
-                    auxUppercase++;
-                    else if(lowercase.test(password[i]))
-                    auxLowercase++;
-                    else if(numbers.test(password[i]))
-                    auxNumber++;
-                    else if(especialChars.test(password[i]))
-                    auxEspecial++;
-                }
-                
-                if (auxUppercase > 0){
-                    if (auxLowercase > 0){
-                        if (auxNumber > 0){
-                            if (auxEspecial) {
-                                console.log(true);
+export default function withValidation(Component) {
+    return ({name,...otherProps}) =>{
+        const [isInvalid, setIsInvalid] = useState(false)
+        
+        const inputTypes = {
+            email: {
+                icon:<MdEmail/>,
+                valid:()=>{},
+            },
+            password: {
+                icon:<RiLockPasswordFill/>,
+                valid:(password)=>{
+                    {
+                        const uppercase = /[A-Z]/;
+                        const lowercase = /[a-z]/; 
+                        const numbers = /[0-9]/;
+                        const specialChars = /[!|@|#|$|%|^|&|*|(|)|-|_]/;
+                        if(password.length > 8){
+                           setIsInvalid(true);
+                        }
+                        if(password.length < 8){
+                            setIsInvalid(true);
+                        }
+                        var auxUppercase = 0;
+                        var auxLowercase = 0;
+                        var auxNumber = 0;
+                        var auxspecial = 0;
+                        for(var i=0; i<password.length; i++){
+                            if(uppercase.test(password[i]))
+                            auxUppercase++;
+                            else if(lowercase.test(password[i]))
+                            auxLowercase++;
+                            else if(numbers.test(password[i]))
+                            auxNumber++;
+                            else if(specialChars.test(password[i]))
+                            auxspecial++;
+                        }
+                        
+                        if (auxUppercase > 0){
+                            if (auxLowercase > 0){
+                                if (auxNumber > 0){
+                                    if (auxspecial) {
+                                        setIsInvalid(false);
+                                    }
+                                }
                             }
                         }
                     }
                 }
-            }
+            },
+            user: {
+                icon:<FaUser/>
+            },
         }
-    },
-    user: {
-        icon:<FaUser/>
-    },
-}
 
-export default function withValidation(Component) {
-    return ({name,...otherProps}) =>{
-        const icon = inputTypes[name]["icon"]??"";   
+        const type = inputTypes[name]??"";
+
         return (
-            <div>
-            <Component {...otherProps} name={name} Icon={icon} onBlur={inputTypes[name]["valid"]}/>
-            </div>)
+            <Component {...otherProps} type={name} name={name} Icon={type.icon} onKeyUp={type && inputTypes[name]["valid"]} valid={isInvalid}/>
+            )
         }
     }
     
