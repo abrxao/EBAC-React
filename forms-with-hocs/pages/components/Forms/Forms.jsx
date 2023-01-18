0baa503e7aa74aa924b3c93194cb3260c5152ca2
Abraão Albuquerque <abraao.hs2@gmail.com>
2023-01-18 15:55:39 -0300

    :sparkles:feat: criando e ajustando 2 formulario, adicionando estilo e criando array que ira receber as respostas

--- forms-with-hocs/pages/components/Forms/Forms.jsx
+++ forms-with-hocs/pages/components/Forms/Forms.jsx
@@ -1,15 +1,74 @@
 import React from "react";
-
+import {GoVerified} from "react-icons/go";
+import {RiRefreshFill} from "react-icons/ri";
+import {RiSendPlaneFill} from "react-icons/ri";
 export default class Forms extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            answers: [],
+            isSend:false,
+            modalErro:false
+        }
+        this._form = React.createRef(); 
+    }
+    
+    handleSubmit(e) {
+        e.preventDefault();
+
+        var answer = {};
+
+        const inputs = this._form.current.querySelectorAll("input");
+
+        inputs.forEach(input => {
+            if(input.classList.contains("invalid") || input.value=="" ){
+                input.classList.add("invalid");
+            }else{
+                const name = input.getAttribute("id");
+                answer[name]=input.value;
+                input.value=""
+            }
+        })
+
+        if(Object.keys(answer).length!=inputs.length){
+            this.setState({modalErro: true});
+            setTimeout(() => {
+                this.setState({modalErro: false});
+            }, 3500);
+        }else{
+            this.state.answers.push(answer);
+            this.setState({isSend: true});
+        }
+    }
+
+    handleReSend(e){
+        e.preventDefault();
+        this.setState({isSend:false});
     }
 
     render(){
         return(
             <div className="formContainer">
-                <form className="formContainer__content">
-                    {this.props.children}
+                <form ref={this._form} className="formContainer__content">
+                    {!this.state.isSend && this.props.children}
+                    {!this.state.isSend && <div><button onClick={e=> this.handleSubmit(e)} type="submit">SEND<RiSendPlaneFill/></button></div>}
+                    {this.state.isSend &&
+                        <div className="formContainer__content__sendArea">
+                            <h2><GoVerified/> SENDED</h2>
+                            <button onClick={e=>this.handleReSend(e)}>SEND AGAIN <RiRefreshFill/></button>
+                            
+                        </div>
+                    }
+                    {this.state.modalErro && 
+                        <div className="formContainer__content__erro">
+                            <div className="formContainer__content__erro__Modal">
+                                <h4>CHECK THE FIELDS IN RED</h4>
+                            </div>
+                            
+                        </div>    
+                    }
+                    
+                    
                 </form>
             </div>
         )
--- forms-with-hocs/pages/components/TextInput/TextInput.jsx
+++ forms-with-hocs/pages/components/TextInput/TextInput.jsx
@@ -1,12 +1,13 @@
-import withValidation from '../utils/withValidation';
-import React,{useState, useEffect} from 'react';
-import {IoMdEye,IoMdEyeOff} from 'react-icons/io'
+import withValidation from './utils/withValidation';
+import React,{useState} from 'react';
+import {IoMdEye,IoMdEyeOff} from 'react-icons/io';
+import {FaExclamationCircle} from 'react-icons/fa';
 
-export function TextInput({Icon, name, onKeyUp, valid, type},props){
+export function TextInput({Icon, name, onKeyUp, valid, type,onKeyPress, maxLength, minLength},props){
     const fnKeyUp = onKeyUp? onKeyUp: ()=>{};
+    const fnKeyPress = onKeyPress?onKeyPress: ()=>{};
     const [isInputFill, setIsInputFill] = new useState(false);
     const [inputType, setInputType] = new useState(type=="password" ? "password" : "text");
-
     const handleKeyDown = (e)=>{
         e.target.value ? setIsInputFill(true): setIsInputFill(false);
     }
@@ -15,14 +16,24 @@ export function TextInput({Icon, name, onKeyUp, valid, type},props){
         
         <div className='textInput'>
 
-            <input 
-            type={inputType}
-            id={name}
-            onChange={e=>handleKeyDown(e)}            
-            className={(isInputFill?"filled":"")+(valid?" invalid":"")}
-            onKeyUp={e=>{fnKeyUp(e.target.value)}}/>
+            <input
+                type={inputType}
+                id={name}
+                onChange={e=>handleKeyDown(e)}            
+                className={(isInputFill?"filled":"")+(valid?" invalid":"")}
+                onKeyUp={e=>{fnKeyUp(e)}}
+                autoComplete={name=="phone"||name=="CPF"?"off":"true"}
+                onBlur={e=>{fnKeyUp(e)}}
+                onKeyDown={e=>{fnKeyPress(e)}}
+            />
 
-            <label htmlFor={name}><span className='textInput__icon'>{Icon}</span>{name[0].toUpperCase() + name.substring(1)}</label>
+            <label htmlFor={name}>
+                <span className='textInput__icon'>{Icon}</span>{name[0].toUpperCase() + name.substring(1)}
+            </label>
+
+            {valid && <div className='textInput__warn'>
+                        <p><FaExclamationCircle/> Please check your {name}.</p>
+                    </div>}
 
             {name === "password" && <span className='textInput__viewPassword' onClick={()=>{setInputType(inputType=="password"?"text":"password")}}>{inputType=="password"?<IoMdEye/>:<IoMdEyeOff/>}</span>}
 
